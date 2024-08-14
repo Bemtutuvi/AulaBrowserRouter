7d0b17b1aaa16fd3db2f99cfc0db3b762aae0715
Arthur Mesquita Redigolo <arthur.redigolo@estudante.ifms.edu.br>
2024-08-14 16:04:10 -0400

    Adicionar Home e Oferta

---
+++ src/home/index.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Heater";
+
+
+export default function Home(){
+    return(
+        <>
+        <Header/>
+        
+        <p>Minha Home</p>
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './home'
+import Oferta from './Oferta'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/oferta" element={<Oferta/>}/>
+
+      </Routes>
+    </BrowserRouter>
     
   </React.StrictMode>,
 )
